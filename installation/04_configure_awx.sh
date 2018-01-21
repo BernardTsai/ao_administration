786e17b64ddbb9d7908af389fcc32767220abbc7
btsai <bernard@tsai.eu>
2018-01-21 17:55:52 +0100

    Allow to reconfigure awx

--- installation/04_configure_awx.sh
+++ installation/04_configure_awx.sh
@@ -16,8 +16,19 @@ echo Defining organization
 
 export HEADER1="Content-Type: application/json"
 export HEADER2="Authorization: Token $TOKEN"
-export DATA='{"name": "DTAG"}'
-export ORGANIZATION=$(curl -s -d "$DATA" -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/organizations/ | jq -r ".id")
+export COUNT=$(curl -s -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/organizations/?search=DTAG | jq -r ".count")
+
+if [ $COUNT -eq 0 ]
+then
+  export HEADER1="Content-Type: application/json"
+  export HEADER2="Authorization: Token $TOKEN"
+  export DATA='{"name": "DTAG"}'
+  export ORGANIZATION=$(curl -s -d "$DATA" -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/organizations/ | jq -r ".id")
+fi
+
+export HEADER1="Content-Type: application/json"
+export HEADER2="Authorization: Token $TOKEN"
+export ORGANIZATION=$(curl -s -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/organizations/?search=DTAG | jq -r ".results[0].id")
 
 echo $ORGANIZATION
 
@@ -27,26 +38,48 @@ echo Defining project
 
 export HEADER1="Content-Type: application/json"
 export HEADER2="Authorization: Token $TOKEN"
-export DATA='{"organization":'$ORGANIZATION', "name":"automation", "scm_type":"git", "scm_url":"http://gitlab/Tools/automation.git"}'
-export PROJECT=$(curl -s -d "$DATA" -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/projects/ | jq -r ".id")
+export COUNT=$(curl -s -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/projects/?search=Automation | jq -r ".count")
+
+if [ $COUNT -eq 0 ]
+then
+  export HEADER1="Content-Type: application/json"
+  export HEADER2="Authorization: Token $TOKEN"
+  export DATA='{"organization":'$ORGANIZATION', "name":"Automation", "scm_type":"git", "scm_url":"http://gitlab/Tools/automation.git"}'
+  export PROJECT=$(curl -s -d "$DATA" -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/projects/ | jq -r ".id")
+
+  sleep 15
+fi
+
+export HEADER1="Content-Type: application/json"
+export HEADER2="Authorization: Token $TOKEN"
+export PROJECT=$(curl -s -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/projects/?search=Automation | jq -r ".results[0].id")
 
 echo $PROJECT
-
-sleep 5
 
 # ------------------------------------------------------------------------------
 
 echo Defining inventory
 
-export INVENTORY_YAML=$(curl -s http://localhost/Tools/environments/raw/master/cloud.yaml)
-
-export INVENTORY_JSON=$(echo "$INVENTORY_YAML" | python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=2)')
-export INVENTORY_DATA=$(echo "$INVENTORY_JSON" | tr -d '\r\n' )
-export INVENTORY_STRING=$(echo "$INVENTORY_DATA" | sed 's/\"/\\"/g' )
 export HEADER1="Content-Type: application/json"
 export HEADER2="Authorization: Token $TOKEN"
-export DATA='{"name":"inventory", "organization":'$ORGANIZATION', "variables":"'$INVENTORY_STRING'"}'
-export INVENTORY=$(curl -s -d "$DATA" -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/inventories/ | jq -r ".id")
+export COUNT=$(curl -s -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/inventories/?search=DTAG | jq -r ".count")
+
+if [ $COUNT -eq 0 ]
+then
+  export INVENTORY_YAML=$(curl -s http://localhost/Tools/environments/raw/master/cloud.yaml)
+
+  export INVENTORY_JSON=$(echo "$INVENTORY_YAML" | python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=2)')
+  export INVENTORY_DATA=$(echo "$INVENTORY_JSON" | tr -d '\r\n' )
+  export INVENTORY_STRING=$(echo "$INVENTORY_DATA" | sed 's/\"/\\"/g' )
+  export HEADER1="Content-Type: application/json"
+  export HEADER2="Authorization: Token $TOKEN"
+  export DATA='{"name":"DTAG Inventory", "organization":'$ORGANIZATION', "variables":"'$INVENTORY_STRING'"}'
+  export INVENTORY=$(curl -s -d "$DATA" -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/inventories/ | jq -r ".id")
+fi
+
+export HEADER1="Content-Type: application/json"
+export HEADER2="Authorization: Token $TOKEN"
+export COUNT=$(curl -s -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/inventories/?search=DTAG | jq -r ".results[0].id")
 
 echo $INVENTORY
 
@@ -57,35 +90,65 @@ echo Defining job templates
 export HEADER1="Content-Type: application/json"
 export HEADER2="Authorization: Token $TOKEN"
 
-export DATA='{"name":"Onboarding", "organization":'$ORGANIZATION', "inventory":'$INVENTORY', "project":'$PROJECT', "playbook":"onboarding-playbook.yml", "ask_variables_on_launch": true, "verbosity": 3}'
-export TEMPLATE=$(curl -s -d "$DATA" -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/job_templates/ | jq -r ".id")
+export COUNT=$(curl -s -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/inventories/?search=Onboarding | jq -r ".count")
 
-echo $TEMPLATE
+if [ $COUNT -eq 0 ]
+then
+  export DATA='{"name":"Onboarding", "organization":'$ORGANIZATION', "inventory":'$INVENTORY', "project":'$PROJECT', "playbook":"onboarding-playbook.yml", "ask_variables_on_launch": true, "verbosity": 3}'
+  export TEMPLATE=$(curl -s -d "$DATA" -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/job_templates/ | jq -r ".id")
 
-export DATA='{"name":"Deployment", "organization":'$ORGANIZATION', "inventory":'$INVENTORY', "project":'$PROJECT', "playbook":"deployment-playbook.yml", "ask_variables_on_launch": true, "verbosity": 3}'
-export TEMPLATE=$(curl -s -d "$DATA" -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/job_templates/ | jq -r ".id")
+  echo $TEMPLATE
+fi
 
-echo $TEMPLATE
+export COUNT=$(curl -s -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/inventories/?search=Deployment | jq -r ".count")
 
-export DATA='{"name":"Inventory", "organization":'$ORGANIZATION', "inventory":'$INVENTORY', "project":'$PROJECT', "playbook":"inventory-playbook.yml", "ask_variables_on_launch": true, "verbosity": 3}'
-export TEMPLATE=$(curl -s -d "$DATA" -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/job_templates/ | jq -r ".id")
+if [ $COUNT -eq 0 ]
+then
+  export DATA='{"name":"Deployment", "organization":'$ORGANIZATION', "inventory":'$INVENTORY', "project":'$PROJECT', "playbook":"deployment-playbook.yml", "ask_variables_on_launch": true, "verbosity": 3}'
+  export TEMPLATE=$(curl -s -d "$DATA" -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/job_templates/ | jq -r ".id")
 
-echo $TEMPLATE
+  echo $TEMPLATE
+fi
 
-export DATA='{"name":"Scale-In", "organization":'$ORGANIZATION', "inventory":'$INVENTORY', "project":'$PROJECT', "playbook":"scale-in-playbook.yml", "ask_variables_on_launch": true, "verbosity": 3}'
-export TEMPLATE=$(curl -s -d "$DATA" -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/job_templates/ | jq -r ".id")
+export COUNT=$(curl -s -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/inventories/?search=Inventory | jq -r ".count")
 
-echo $TEMPLATE
+if [ $COUNT -eq 0 ]
+then
+  export DATA='{"name":"Inventory", "organization":'$ORGANIZATION', "inventory":'$INVENTORY', "project":'$PROJECT', "playbook":"inventory-playbook.yml", "ask_variables_on_launch": true, "verbosity": 3}'
+  export TEMPLATE=$(curl -s -d "$DATA" -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/job_templates/ | jq -r ".id")
 
-export DATA='{"name":"Scale-Out", "organization":'$ORGANIZATION', "inventory":'$INVENTORY', "project":'$PROJECT', "playbook":"scale-out-playbook.yml", "ask_variables_on_launch": true, "verbosity": 3}'
-export TEMPLATE=$(curl -s -d "$DATA" -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/job_templates/ | jq -r ".id")
+  echo $TEMPLATE
+fi
 
-echo $TEMPLATE
+export COUNT=$(curl -s -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/inventories/?search=Scale-In | jq -r ".count")
 
-export DATA='{"name":"Cleanup", "organization":'$ORGANIZATION', "inventory":'$INVENTORY', "project":'$PROJECT', "playbook":"cleanup-playbook.yml", "ask_variables_on_launch": true, "verbosity": 3}'
-export TEMPLATE=$(curl -s -d "$DATA" -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/job_templates/ | jq -r ".id")
+if [ $COUNT -eq 0 ]
+then
+  export DATA='{"name":"Scale-In", "organization":'$ORGANIZATION', "inventory":'$INVENTORY', "project":'$PROJECT', "playbook":"scale-in-playbook.yml", "ask_variables_on_launch": true, "verbosity": 3}'
+  export TEMPLATE=$(curl -s -d "$DATA" -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/job_templates/ | jq -r ".id")
 
-echo $TEMPLATE
+  echo $TEMPLATE
+fi
+
+export COUNT=$(curl -s -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/inventories/?search=Scale-Out | jq -r ".count")
+
+if [ $COUNT -eq 0 ]
+then
+  export DATA='{"name":"Scale-Out", "organization":'$ORGANIZATION', "inventory":'$INVENTORY', "project":'$PROJECT', "playbook":"scale-out-playbook.yml", "ask_variables_on_launch": true, "verbosity": 3}'
+  export TEMPLATE=$(curl -s -d "$DATA" -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/job_templates/ | jq -r ".id")
+
+  echo $TEMPLATE
+fi
+
+export COUNT=$(curl -s -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/inventories/?search=Cleanup | jq -r ".count")
+
+if [ $COUNT -eq 0 ]
+then
+  export DATA='{"name":"Cleanup", "organization":'$ORGANIZATION', "inventory":'$INVENTORY', "project":'$PROJECT', "playbook":"cleanup-playbook.yml", "ask_variables_on_launch": true, "verbosity": 3}'
+  export TEMPLATE=$(curl -s -d "$DATA" -H "$HEADER1" -H "$HEADER2" http://localhost:81/api/v2/job_templates/ | jq -r ".id")
+
+  echo $TEMPLATE
+fi
 
 # ------------------------------------------------------------------------------
 
